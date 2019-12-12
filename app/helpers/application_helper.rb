973e9574b506a7512401660bb4abbe7a208345c6
Jim Kratus <32602483+jkrat@users.noreply.github.com>
2019-12-12 13:01:23 -0600

    app layout, shared views, helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns a BEM list of classes for a given controller/action combo
+  def body_classes(controller, action)
+    controller = controller.tr('/', '_')
+
+    classes = []
+    classes << "#{controller}__body"
+    classes << "#{controller}__body--#{action}"
+    classes.join(' ')
+  end
 end
